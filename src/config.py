1ce99943feab755b4ec9c635499e80c497b814bb
Daniel Johansen Trosten <daniel.johansen.trosten@vegvesen.no>
2020-02-19 13:09:38 +0100

    Docs-compatible comments in config

--- src/config.py
+++ src/config.py
@@ -4,28 +4,33 @@ Configuration variables.
 import os
 
 
-PROJECT_ROOT = r"C:\Users\dantro\Repos\image-anonymisation"
+#: Root directory for the project
+PROJECT_ROOT = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
+#: Directory containing saved mdoels
 GRAPH_DIRECTORY = os.path.join(PROJECT_ROOT, "graphs")
 
+#: Name of the masking model
 MODEL_NAME = 'mask_rcnn_inception_resnet_v2_atrous_coco_2018_01_28'
 # MODEL_NAME = 'mask_rcnn_inception_v2_coco_2018_01_28'
 # MODEL_NAME = 'mask_rcnn_inception_resnet_v2_atrous_coco_2018_01_28'
 # MODEL_NAME = 'ssd_mobilenet_v1_coco_2017_11_17'
+
+#: Full path to the saved model
 MODEL_PATH = os.path.join(GRAPH_DIRECTORY, MODEL_NAME)
 
+#: Base URL for model downloading
 DOWNLOAD_BASE = 'http://download.tensorflow.org/models/object_detection/'
 
-# Path to frozen detection graph. This is the actual model that is used for the object detection.
+#: Path to frozen detection graph. This is the actual model that is used for the object detection.
 PATH_TO_FROZEN_GRAPH = os.path.join(MODEL_PATH, 'frozen_inference_graph.pb')
 
-# List of the strings that is used to add correct label for each box.
+#: List of the strings that is used to add correct label for each box.
 PATH_TO_LABELS = 'mscoco_label_map.pbtxt'
 
-# COCO labels to mask in input images
+#: COCO labels to mask in input images
 MASK_LABELS = (1, 2, 3, 4, 6, 8)
 
-# Masking colors <label id>: <RGB color>
-DEFAULT_COLOR = (100, 100, 100)
+#: Masking colors. <COCO label id>: <RGB color>
 LABEL_COLORS = {
     1: (255, 255, 255),
     2: (0, 0, 255),
@@ -34,3 +39,5 @@ LABEL_COLORS = {
     6: (0, 255, 255),
     8: (0, 255, 0)
 }
+#: Default color for labels not contained in `LABEL_COLORS`
+DEFAULT_COLOR = (100, 100, 100)
